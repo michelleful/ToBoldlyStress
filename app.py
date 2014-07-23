71640a23934f9a5b6f4e748caa713834f8c83db4
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-07-23 00:13:44 -0400

    Slightly more sophisticated Flask app

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('base.html')
 
