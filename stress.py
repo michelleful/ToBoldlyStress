d2c2870ebab7edb8cdb849be51dfb9a486934444
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-07-22 21:39:27 -0400

    Shuffled off processing of individual words into a separate function

--- stress.py
+++ stress.py
@@ -20,7 +20,11 @@ STRESS_DICT = load_dictionary()
         
 word_re = re.compile(r"\b[A-Za-z']+\b")
 
+def process_word(word):
+    # TODO: match case
+    return STRESS_DICT[word.upper()]
+
 def process_text(text):
-    return word_re.sub(lambda match: STRESS_DICT[match.group().upper()], text)
+    return word_re.sub(lambda match: process_word(match.group()), text)
     
-print process_text("Hello world. Look who's talking!")
+
