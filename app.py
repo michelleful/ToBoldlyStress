afd73a2d086678157a52ff2ddf94a65209414ddc
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-07-23 08:45:39 -0400

    Basic form handling view function

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from stress import process_text
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('base.html')
+@app.route('/', methods=['POST','GET'])
+def main():
+    if request.method == 'POST' and len(request.form['text'].strip()):
+        return render_template('response.html',
+                               stressed=process_text(request.form['text']))
+    return render_template('form.html')
+
 
