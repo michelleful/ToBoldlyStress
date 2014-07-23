8a357f7226df7ae55fc7ae2d27178a450eeb7fa8
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-07-23 10:31:28 -0400

    Unbold single syllables as it's too visually cluttered - fixes #4

--- stress.py
+++ stress.py
@@ -3,6 +3,8 @@ Given some text, return the stressed spelling version
 """
 import re
 
+BOLD_SINGLE_SYLLABLE = False
+
 # Tricky parts: ignore commas, double quotes, etc while leaving 's alone
 #               match the case in returning the stressed spelling
 
@@ -13,23 +15,34 @@ def load_dictionary():
     with open(STRESS_DICT_PATH, 'r') as f:
         for line in f:
             word, stressed_word = line.strip().split('\t')
-            dictionary[word.strip()] = stressed_word
+            dictionary[word.strip()] = stressed_word.strip()
     return dictionary
     
 STRESS_DICT = load_dictionary()
-        
-word_re = re.compile(r"\b[A-Za-z\-']+\b")
 
 def process_word(word):
+    """Look up stressed form of word, and perform various case manipulations
+       etc."""
     if word.upper() not in STRESS_DICT:
         return word
+
     stressed = STRESS_DICT[word.upper()]
+
+    # unbold single syllables
+    if not BOLD_SINGLE_SYLLABLE:
+        if stressed.startswith('<b>') and stressed.endswith('</b>'):
+            return word
+
     if word.isupper():  # word is capitalized
         return stressed.upper().replace('B>','b>')
     elif word[0].islower():  # the whole word should be lower case
         return stressed.lower()
     else:
         return stressed  # sentence case is the default
+
+
+# regular expression for extracting words
+word_re = re.compile(r"\b[A-Za-z\-']+\b")
 
 def process_text(text):
     """Main function to be called in this module. Given some plain text
