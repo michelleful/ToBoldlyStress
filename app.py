4e20a72db8dbf6b4ca1d91284482e3cc6525d183
Michelle Fullwood <michelle.fullwood@gmail.com>
2014-07-24 19:12:55 -0400

    Added some markdown massaging so that newlines are respected

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from stress import process_text
 
 app = Flask(__name__)
 
+def process_and_markdown(text):
+    # 1. run process_text and get stressed output
+    # 2. insert two spaces before newlines so that lines are maintained
+    # 3. run through markdown
+    return Markup(markdown.markdown(
+                  process_text(request.form['text']).replace("\n", "  \n")))
+
 @app.route('/', methods=['POST','GET'])
 def main():
     if request.method == 'POST' and len(request.form['text'].strip()):
         return render_template('response.html',
-                               stressed=Markup(markdown.markdown(process_text(request.form['text']))))
+                               stressed=process_and_markdown(request.form['text']))
     return render_template('form.html')
 
 
